55f575bb6c25091c55d2ff96a680067a2cbf1fa0
stephan.com <stephan@stephan.com>
2019-02-02 01:22:48 -0500

    redis cache rendered QR

--- qrinator.go
+++ qrinator.go
@@ -7,6 +7,7 @@ import (
   "image"
   "strconv"
   "net/http"
+  "github.com/go-redis/redis"
   "github.com/disintegration/imaging"
   qrcode "github.com/skip2/go-qrcode"
 )
@@ -18,8 +19,16 @@ const Offset  = Size / 3
 const Inset   = (Size - Offset) / 2
 
 var logo image.Image
+var redisdb *redis.Client
 
 func init() {
+  log.Println("init redis")
+  redisdb = redis.NewClient(&redis.Options{
+    Addr:     "localhost:6379",
+    Password: "", // no password set
+    DB:       0,  // use default DB
+  })
+
   log.Println("read logo")
   response, _ := http.Get(LogoUrl)
   defer response.Body.Close()
@@ -38,18 +47,34 @@ func url(payload string) string {
 }
 
 func buildQr(payload string) image.Image {
-  img, _ := qrcode.New(url(payload), qrcode.Medium)
-  image := img.Image(Size)
-  image = imaging.OverlayCenter(image, logo, 1.0)
-  return image
+    img, _ := qrcode.New(url(payload), qrcode.Medium)
+    image := img.Image(Size)
+    image = imaging.OverlayCenter(image, logo, 1.0)
+    return image
+}
+
+func cachePng(payload string) *bytes.Buffer {
+  cached, err := redisdb.Get(payload).Result()
+  png := new(bytes.Buffer)
+
+  if err == redis.Nil {
+    log.Println("cache miss, generating "+payload)
+    imaging.Encode(png, buildQr(payload), imaging.PNG)
+    redisdb.Set(payload, png.String(), 0)
+  } else {
+    png = bytes.NewBufferString(cached)
+  }
+
+  return png
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
   if r.Method == "DELETE" {
+    log.Println("clear cache")
     fmt.Fprintf(w, "clear cache")
+    redisdb.FlushDB()
   } else {
-    buffer := new(bytes.Buffer)
-    imaging.Encode(buffer, buildQr(r.URL.Path), imaging.PNG)
+    buffer := cachePng(r.URL.Path)
 
     w.Header().Set("Content-Type", "image/png")
     w.Header().Set("Content-Length", strconv.Itoa(buffer.Len()))
